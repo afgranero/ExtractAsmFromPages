9f5697b830bdfe5eee753b43831e91e00cf262bc
Airton da Fonseca Granero <agranero@gmail.com>
2026-03-11 19:47:49 -0300

    Corrected some assembler tests.

--- assembler_tests/test11-j.asm
+++ assembler_tests/test11-j.asm
@@ -1,8 +1,5 @@
 ; testing if relative jumps specified as abolute addresses are detected as error if jumps outside the program
  ORG 0000H: JR 0006H
- ORG 0001H: NOP
  ORG 0002H: NOP
  ORG 0003H: NOP
  ORG 0004H: NOP
- ORG 0005H: NOP
-
